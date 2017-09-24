9bc3a9b0865055cda80d10eacd624c16c3dff557
kpagels <kelvin@pagels.dk>
2017-09-24 09:06:17 +0200

    Updating main

--- EmbeddedSystemX/EmbeddedSystemX/main.cpp
+++ EmbeddedSystemX/EmbeddedSystemX/main.cpp
@@ -19,26 +19,10 @@ void sleep(clock_t wait)
 
 int main(int argc, char* argv[])
 {
-	cout << "What a turttle" << endl;
-	cout << "hello world" << endl;
+	cout << "Starting program" << endl;
 	EmbeddedSystemX *Embed1 = new EmbeddedSystemX();
-	int count = 0, i = 0;
-	int seconds;
+	
 
-	while (1)
-	{
-		if (i % 3 == 0)
-			printf("---------\nSession %d\n---------\n", ((i + 1) / 3) + 1);
-
-		if (count == 0) seconds = 6, count = 1;
-		else if (count == 1) seconds = 4, count = 2;
-		else if (count == 2) seconds = 5, count = 0;
-
-		sleep((clock_t)seconds * CLOCKS_PER_SEC);
-		Embed1->Handle();
-
-		i++;
-	}
 	return 0;
 }
 
